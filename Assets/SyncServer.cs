3b4af2b1e93e4451d6f7a1d9d7086ad5f5ed4ee1
Nick Young <nyou045@UoA.auckland.ac.nz>
2017-10-04 12:03:18 +1300

    manually position, always face text to user

--- Assets/SyncServer.cs
+++ Assets/SyncServer.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VR.WSA.Input;
 
 #if WINDOWS_UWP
 using Windows.Networking.Sockets;
@@ -15,6 +16,10 @@ public class SyncServer : MonoBehaviour
 {
 	public Text target;
 	private string text = "Waiting for server...";
+	private GestureRecognizer ManipulationRecognizer;
+	private Vector3 manipulationPreviousPosition = Vector3.zero;
+	private bool manuallyPositioned = false;
+
 #if WINDOWS_UWP
 	private DatagramSocket socket;
 	
@@ -47,15 +52,35 @@ public class SyncServer : MonoBehaviour
 #endif
 	private void Update()
 	{
+		target.text = text;
 		var c = Camera.main.transform;
+		var t = target.transform.parent.transform;
+		t.localRotation = c.localRotation;
+		if (manuallyPositioned) return;
 		RaycastHit hit;
 		Physics.Raycast(c.position, c.forward, out hit);
 		if (hit.distance > 0)
 		{
-			var t = target.transform.parent.transform;
-			t.localPosition = new Vector3(t.localPosition.x, t.localPosition.y, hit.distance);
+			t.localPosition = c.localPosition;
+			t.localPosition += c.forward * hit.distance;
 			t.localScale = Vector3.one * .0005f * hit.distance;
 		}
-		target.text = text;
+	}
+
+	private void Awake()
+	{
+		ManipulationRecognizer = new GestureRecognizer();
+		ManipulationRecognizer.SetRecognizableGestures(GestureSettings.ManipulationTranslate);
+		ManipulationRecognizer.ManipulationUpdatedEvent += ManipulationRecognizer_ManipulationUpdatedEvent;
+		ManipulationRecognizer.StartCapturingGestures();
+	}
+
+	private void ManipulationRecognizer_ManipulationUpdatedEvent(InteractionSourceKind source, Vector3 position, Ray ray)
+	{
+		var t = target.transform.parent.transform;
+		var moveVector = position - manipulationPreviousPosition;
+		manipulationPreviousPosition = position;
+		t.localPosition += moveVector * 2;
+		manuallyPositioned = true;
 	}
 }
